e881e7c3e1f08f16ab18e5f19ed6a581b4bf51a3
ericksalignac <ericksalignac@gmail.com>
2022-07-20 11:15:24 -0300

    Seção 7 - Template Engine - 9/15 alas concluídas

---
+++ 7_TEMPLATE_ENGINE/3_dados_para_view/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+
+  const user = {
+    name: 'Matheus',
+    surname: 'Battisti',
+    age: 30,
+  }
+
+  const palavra = "Teste"
+
+  res.render('home', {user: user, palavra, auth})
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
